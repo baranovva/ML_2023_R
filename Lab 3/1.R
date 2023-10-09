5c1867eeec01c9ee13342da89199e27cf2a41730
Vasillii <vasya.ba01@gmail.com>
2023-10-09 15:24:26 +0300

    Initial commit

---
+++ Lab 3/1.R
@@ -0,0 +1,23 @@
+library(e1071)
+
+data_train <- read.table("Lab 3/svmdata1.txt", sep = "\t", stringsAsFactors = TRUE)
+data_test <- read.table("Lab 3/svmdata1test.txt", sep = "\t", stringsAsFactors = TRUE)
+
+SVM <- svm(Color ~ ., data = data_train, type = "C-classification", cost = 1, kerner = "linear")
+SVM
+
+plot(SVM, data_train, grid = 250, symbolPallete = c("Green", "Red"))
+
+n_row <- dim(data_test)[1]
+
+res1_train <- predict(SVM, data_train)
+result <- table(data_train$Color, res1_train)
+accuracy <- tr(result) / n_row
+accuracy
+
+res1_test <- predict(SVM, data_test)
+result <- table(data_test$Color, res1_test)
+accuracy <- tr(result) / n_row
+accuracy
+
+
